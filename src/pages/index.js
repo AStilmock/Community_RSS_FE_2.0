b1f3464a1f10eac55a7e088ae67c43e902f1252d
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-01-24 16:47:31 -0700

    removed link to about page in body, added image to page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 import * as React from "react"
 import { Link } from 'gatsby'
 import Layout from '/src/components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Welcome to Community RSS, the Community Resource Rating Website!">
       <p>Simply enter a valid United States zip code below to view all local resource data.</p>
-      <Link to="/about">About</Link>
+      <StaticImage
+        alt="I know kung fu"
+        src="/Users/stillymonster/Desktop/Screen Shots/Screen Shot 2023-06-02 at 9.00.28 AM.png"
+      />
     </Layout>
   )
 }
