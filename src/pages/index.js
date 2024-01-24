69d9842bc23be16b6ea1a09980412b60632cde31
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-01-24 15:33:15 -0700

    layout component added to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,13 @@
 import * as React from "react"
 import { Link } from 'gatsby'
+import Layout from '/src/components/layout'
 
 const IndexPage = () => {
   return (
-    <main>
-      {/* <div
-        style={{
-          backgroundColor: '#5E2C4D',
-          width: '1000px',
-          height: '1000px'
-        }}
-      /> */}
-      <h1>Welcome to Community RSS, the Community Resource Rating Website!</h1>
-      <p>Simply enter a valid United States zip code to view all local resource data.</p>
+    <Layout pageTitle="Welcome to Community RSS, the Community Resource Rating Website!">
+      <p>Simply enter a valid United States zip code below to view all local resource data.</p>
       <Link to="/about">About</Link>
-    </main>
+    </Layout>
   )
 }
 
