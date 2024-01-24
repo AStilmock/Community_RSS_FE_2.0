ba7bf00561be7266d0875a7e882809f43b3b85f3
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-01-24 15:09:50 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Community_RSS_FE_2.0`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
