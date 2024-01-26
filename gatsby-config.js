c16a0fb7e9c4d7a620e65012f316ae5ea0f2de8e
Andrew Stilmock <andrew.stilmock@gmail.com>
2024-01-26 16:02:30 -0700

    updated names to match, resolved error with naming convention

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,8 +12,8 @@ module.exports = {
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        name: `blog`,
-        path: `${__dirname}/blog`,
+        name: `search`,
+        path: `${__dirname}/search_files`,
       }
     },
     // "gatsby-transformer-json",
